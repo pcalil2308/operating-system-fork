031efe5a6c38a340f3661a71e143424ca07550b5
rav-andre <aoravagnani@gmail.com>
2025-09-06 11:51:58 -0300

    simple adjustments

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,11 +6,11 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 var fs = require("fs");
-// const educationCenterDeploy = require("../migrations/1_educationCenter.deploy.js");
-// const sintropAppStoreDeploy = require("../migrations/2_sintropAppStore.deploy.js");
-// const globalPlantCatalogDeploy = require("../migrations/3_globalPlantCatalog.js");
-// const rcTestRewardDeploy = require("../migrations/4_rcTestReward.js");
-// const humansPeaceTreatyDeploy = require("../migrations/5_humansPeaceTreaty.js");
+const educationCenterDeploy = require("../migrations/1_educationCenter.deploy.js");
+const sintropAppStoreDeploy = require("../migrations/2_sintropAppStore.deploy.js");
+const globalPlantCatalogDeploy = require("../migrations/3_globalPlantCatalog.js");
+const humansPeaceTreatyDeploy = require("../migrations/5_humansPeaceTreaty.js");
+const whitepaperDeploy = require("../migrations/6_whitepaperCenter.js");
 
 const sleep = (ms = 0) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -50,11 +50,12 @@ function showDeployedAddress() {
 async function main() {
   await startDeployAlert();
 
-  // await educationCenterDeploy();
-  // await sintropAppStoreDeploy();
-  // await globalPlantCatalogDeploy();
+  await educationCenterDeploy();
+  await sintropAppStoreDeploy();
+  await globalPlantCatalogDeploy();
   // await rcTestRewardDeploy();
   await humansPeaceTreatyDeploy();
+  await whitepaperDeploy();
 
 //  await afterDeploy();
 
